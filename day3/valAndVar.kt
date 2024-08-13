483a507e0fed0b9e1a21ebb95407824add2ea4f6
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-13 21:33:15 +0530

    Val and Var

---
+++ day3/valAndVar.kt
@@ -0,0 +1,10 @@
+class myClass{
+    companion object{
+        @JvmStatic
+        fun main(args : Array<String>){
+            var a= 1
+            val b= "Sachin singh"
+            print("hello $a,$b")
+        }
+    }
+}
