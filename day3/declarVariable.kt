1f4700623e752db52b4835eba3dc4363b779e42d
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-13 20:38:54 +0530

    Declare Variable in  kotlin

---
+++ day3/declarVariable.kt
@@ -0,0 +1,10 @@
+
+    class myclass{
+        companion object {
+            @JvmStatic
+            fun main(args:Array<String>){
+                var a:Int = 7
+                print("Hello world $a")
+            }
+        }
+    }
