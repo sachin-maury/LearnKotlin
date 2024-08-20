f256e0a9ceb513616dbed8aff336d9b2fbf785b9
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-21 00:22:05 +0530

    datatype

---
+++ day5/dataType.kt
@@ -0,0 +1,14 @@
+fun main(){
+    
+      var name :Int
+      nameInt=1445
+      println(nameInt)
+  
+      var myByte:Byte
+      myByte=127
+      println(myByte)
+  
+      var myShort:Short
+      myShort=4352
+      println(myShort)
+  }
