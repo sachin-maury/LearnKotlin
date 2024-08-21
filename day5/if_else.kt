c40b86a2464c068cda4e308e216c988228e846c9
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-21 14:15:49 +0530

    if else condition

---
+++ day5/if_else.kt
@@ -0,0 +1,11 @@
+fun main(){
+   
+       val tusharHeight =5.5
+       val sachinHeight = 5.8
+       if (tusharHeight > sachinHeight){
+           println("tushar is taller")
+       }
+       else{
+           println("sachin is taller")
+       }
+   }
