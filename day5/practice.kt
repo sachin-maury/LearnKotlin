95e70aa19e255a027d4d0ff95d0a12d163625795
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-21 14:53:09 +0530

    when

---
+++ day5/practice.kt
@@ -0,0 +1,14 @@
+
+
+fun main(){
+    val age =89
+    when(age){
+        65 ->  println("You are eligible for voting")
+        45 -> println("You are eligible for voting ")
+        20 -> println("You are  take permissoin  for voting ")
+        89 -> println("You are eligible for voting .89 ")
+        55 -> println("You are eligible for voting ")
+        12-> println("You are Not eligible for voting ")
+
+    }
+}
