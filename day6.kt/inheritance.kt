506b7b26102da15e19858536afa1ae1c9a2ebdc9
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-22 22:35:55 +0530

    inheritance

---
+++ day6.kt/inheritance.kt
@@ -0,0 +1,21 @@
+
+
+class Animal{
+    fun cow(){
+        println("Cow is eating grass")
+    }
+    }
+class Dog {
+    fun bark(){
+        println("dog is barking at a night")
+    }
+}
+
+
+fun main() {
+    val obj=Animal()
+    obj.cow()
+    val obj1=Dog()
+    obj1.bark()
+
+}
