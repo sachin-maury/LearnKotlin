3a3044ee4d5d78505053fdc50c6ade7d60e639d8
Sachin Maurya <kumarsachin67671@gmail.com>
2024-09-30 09:59:43 +0530

    Method Overriding Run Time Plolymerphism

---
+++ day7/runtimePolymerphism.kt
@@ -0,0 +1,26 @@
+fun main(){
+    val classB=ClassB()
+    val classA= ClassA()
+    println("The name is :" +classB.name + ","+ classA.name)
+    println("The sum is :" +classB.Add(6,8)+","+classA.Add(4,2))
+    println("")
+
+}
+
+ open  class ClassA {
+  open  var name = "sachin"
+  open  fun Add(a :Int,b:Int):Int{
+         return a+b
+    }
+}
+class ClassB : ClassA(){
+    override var name :String ="sonu"
+ override  fun Add(a:Int,b:Int):Int{
+        val sum =super.Add(a,b)
+        val sqr = sum*sum
+        return sqr
+    }
+
+
+
+}
