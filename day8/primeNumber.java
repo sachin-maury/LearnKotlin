67bb4ba5631b277786d8b57ddb4bacd8b232f526
Sachin Maurya <kumarsachin67671@gmail.com>
2024-10-01 21:58:51 +0530

    Prime Number code

---
+++ day8/primeNumber.java
@@ -0,0 +1,30 @@
+public class primeNumber {
+
+    public static void main(String[] args) {
+        
+                System.out.println("Prime numbers from 1 to 100 are:");
+                printPrimesUpTo100();
+            }
+
+            public static void printPrimesUpTo100() {
+                for (int i = 2; i <= 100; i++) {
+                    if (isPrime(i)) {
+                        System.out.print(i + " ");
+                    }
+                }
+            }
+
+            public static boolean isPrime(int num) {
+                if (num <= 1) {
+                    return false;
+                }
+                for (int i = 2; i <= Math.sqrt(num); i++) {
+                    if (num % i == 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        
+
+    }
