f2cc1269150ae9c745e7eea4adf32d7db46adec9
Sachin Maurya <kumarsachin67671@gmail.com>
2024-10-07 21:25:13 +0530

    prime

---
+++ day8/prime3.java
@@ -0,0 +1,29 @@
+public class prime3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Prime numbers from 1 to 100 are:");
+        printPrimesUpTo100();
+    }
+
+    public static void printPrimesUpTo100() {
+        for (int i = 2; i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
