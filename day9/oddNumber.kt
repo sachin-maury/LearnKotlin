c272238ce1c67e32927e8251a0fc4ec9eb6ebf20
Sachin Maurya <kumarsachin67671@gmail.com>
2024-10-15 10:32:15 +0530

    odd num

---
+++ day9/oddNumber.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Enter your number")
+    val num = readln()!!.toInt()
+    println("Your number is : ")
+
+    if(num != 2)
+        println("This number is odd : ")
+    
+    else
+        println("This number is :")
+}
