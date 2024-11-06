2af609741acf13f8fbfafa0acb0d0df9a7a0f541
Sachin Maurya <kumarsachin67671@gmail.com>
2024-11-06 23:20:52 +0530

    This keyboard and Super keyword

--- day10/This.kt
+++ day10/This.kt
@@ -7,12 +7,33 @@
          }
          
           open class Car{
+            var ManufactureNo =1010101
          
+            fun PressBrakes(){
+                println("Car has been stoped")
+                
+            }
+            fun PressAccln(speed :Int){
+                println("Car acclerated with " + speed + "km/hm")
+            }
+
+            fun ChangeGear(gear:Int){
+                println("Gear shifted to " + gear  )
+            }
+
          }
          
          
          class audi:Car(){
              var chasisNo = 1000
+             init{
+                println("Audi Created"+ super.ManufactureNo)
+                println("Step1:"+ super.ChangeGear(1))
+                println("Step2:"+ super.PressAccln(10))
+                println("Step3:"+ super.ChangeGear(2))
+                println("Step4:"+ super.PressBrakes())
+                
+             }
              fun interior(chasisNo:Int){
                  println("Chasisno is:"+this.chasisNo)
              }
