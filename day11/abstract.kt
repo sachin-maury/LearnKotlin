484573bafa1f6e6a9b0957f0ddbac52c6bf9fb99
Sachin Maurya <kumarsachin67671@gmail.com>
2024-11-08 22:51:01 +0530

    abstract

---
+++ day11/abstract.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+}
+
+abstract class AbClass{
+    var name:String ="Jhon"
+    abstract var branch: String
+    
+    fun Add(a:Int,b:Int):Int{
+        return a+b
+    }
+    abstract fun oprate(a:Int,b:Int)
+}
